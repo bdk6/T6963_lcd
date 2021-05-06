67091b20cb0d1479dd0dd1460e16634dbdbd72ff
Will Cooke <wrcooke@wrcooke.net>
2021-05-06 06:19:48 -0500

    Created Arduino directory and added files

---
+++ Arduino/T6963_lcd/T6963_lcd.ino
@@ -0,0 +1,31 @@
+////////////////////////////////////////////////////////
+/// @file T6963_lcd.ino
+/// @copy Copyright (C) 2021 Will Cooke
+/// @brief Controls 240x64 LCD display using T6963 
+////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+
+bool ports_init()
+{
+  bool rtn = false;
+  // set /ce to high
+  // set data pins to input
+  // set rd / wr to high
+  // set c/d to command
+  // set /res to high
+  
+
+  return rtn;
+}
+void setup() 
+{
+  // put your setup code here, to run once:
+
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+
+}
